d05347995cffad0a967b99be69ca71de83ce9366
Aleksey Mischanchuk <groovypong@gmail.com>
2023-05-08 12:59:54 +0300

    Added CalculateServiceTest

---
+++ src/test/java/com/neoflex/VacationPaymentsCalculator/calculate/CalculateServiceTest.java
@@ -0,0 +1,21 @@
+package com.neoflex.VacationPaymentsCalculator.calculate;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculateServiceTest {
+
+    private final CalculateService calculateService = new CalculateService();
+
+    @Test
+    public void calculateShouldReturnBigDecimalResult() {
+        String salary = "25";
+        String vacationDays = "1";
+        String expectedResult = "1.14";
+
+        assertEquals(new BigDecimal("1.14"), calculateService.calculate(salary, vacationDays));
+    }
+}
